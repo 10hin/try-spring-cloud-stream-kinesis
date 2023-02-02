bd796fd15043dd849eb637384efa1d6d8904df06
10hin <10hin@outlook.jp>
2023-02-02 20:48:25 +0900

    first subscription succeed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,23 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public Consumer<String> log() {
+		return message -> {
+			System.out.println("Received: \"" + message + "\"");
+		};
+	}
+
+//	public static class Person {
+//		private String name;
+//		public String getName() {
+//			return name;
+//		}
+//		public void setName(String name) {
+//			this.name = name;
+//		}
+//		public String toString() {
+//			return this.name;
+//		}
+//	}
 }
